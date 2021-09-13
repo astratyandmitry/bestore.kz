92e593e841e8deb516e07f2cd1082717c5235e10
Astratyan Dmitry <astratyandmitry@gmail.com>
2021-09-13 22:27:09 +0600

    FIX

--- resources/domain/shop/views/home/partials/banners-split.blade.php
+++ resources/domain/shop/views/home/partials/banners-split.blade.php
@@ -6,7 +6,8 @@
       <div class="banners-split__grid">
         @foreach($splitBanners as $splitBanner)
           <a href="{{ $splitBanner->url }}" target="_blank" class="banners-split__item">
-            <img src="{{ $splitBanner->image }}" alt="{{ $splitBanner->title }}">
+            <img src="{{ $splitBanner->image }}" alt="{{ $splitBanner->title }}" class="banners-split__image">
+            <img src="{{ $splitBanner->image }}" alt="{{ $splitBanner->title }}" class="banners-split__image banners-split__image--mobile">
           </a>
         @endforeach
       </div>
